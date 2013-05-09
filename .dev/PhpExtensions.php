965caf2eeaa2c62b4927f8797a6c8894ac4bc7ba
Amy Stephen <AmyStephen@gmail.com>
2013-05-08 20:01:13 -0500

    Symfony approach to installing php extensions

--- .dev/PhpExtensions.php
+++ .dev/PhpExtensions.php
@@ -1,13 +1,13 @@
 <?php
 
 /*
-* This file is part of the Symfony package.
-*
-* (c) Fabien Potencier <fabien@symfony.com>
-*
-* For the full copyright and license information, please view the LICENSE
-* file that was distributed with this source code.
-*/
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 /**
  * Install PHP extensions for Travis CI.
@@ -31,23 +31,29 @@ class PhpExtensions
         $this->phpVersion = phpversion();
         $this->iniPath = php_ini_loaded_file();
         $this->extensions = array(
+            'memcache' => array(
+                'url'        => 'http://pecl.php.net/get/memcache-2.2.6.tgz',
+                'php_version' => array(),
+                'cfg'         => array('--enable-memcache'),
+                'ini'         => array('extension=memcache.so'),
+            ),
             'memcached' => array(
-                'url' => 'http://pecl.php.net/get/memcached-1.0.2.tgz',
+                'url'        => 'http://pecl.php.net/get/memcached-1.0.2.tgz',
                 'php_version' => array(
                     // memcached 1.0.2 does not build on PHP 5.4
                     array('<', '5.4'),
                 ),
-                'cfg' => array(),
-                'ini' => array('extension=memcached.so'),
+                'cfg'         => array(),
+                'ini'         => array('extension=memcached.so'),
             ),
             'apc' => array(
-                'url' => 'http://pecl.php.net/get/APC-3.1.9.tgz',
+                'url'        => 'http://pecl.php.net/get/APC-3.1.9.tgz',
                 'php_version' => array(
                     // apc 3.1.9 causes a segfault on PHP 5.4
                     array('<', '5.4'),
                 ),
-                'cfg' => array(),
-                'ini' => array(
+                'cfg'         => array(),
+                'ini'         => array(
                     'extension=apc.so',
                     'apc.enabled=1',
                     'apc.enable_cli=1',
@@ -56,13 +62,13 @@ class PhpExtensions
                 ),
             ),
             'xcache' => array(
-                'url' => 'http://xcache.lighttpd.net/pub/Releases/1.2.2/xcache-1.2.2.tar.gz',
+                'url'        => 'http://xcache.lighttpd.net/pub/Releases/1.2.2/xcache-1.2.2.tar.gz',
                 'php_version' => array(
                     // xcache does not build with Travis CI (as of 2012-01-09)
                     array('<', '5'),
                 ),
-                'cfg' => array('--enable-xcache'),
-                'ini' => array(
+                'cfg'         => array('--enable-xcache'),
+                'ini'         => array(
                     'extension=xcache.so',
                     'xcache.admin.enable_auth=0',
                     'xcache.var_size=1M',
