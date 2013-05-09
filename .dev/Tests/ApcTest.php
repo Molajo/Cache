9971fe15568f2e8b0957fc31eb5bfdda4440ce5f
Amy Stephen <AmyStephen@gmail.com>
2013-05-08 20:17:07 -0500

    Add more tests

--- .dev/Tests/ApcTest.php
+++ .dev/Tests/ApcTest.php
@@ -94,6 +94,171 @@ class ApcTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($value, $results->getValue());
     }
 
+    /**
+     * Create a cache entry
+     *
+     * @covers  Molajo\Cache\Handler\File::connect
+     *
+     * @return  $this
+     * @since   1.0
+     */
+    public function testSet()
+    {
+        $value = 'Stuff';
+        $key   = md5($value);
+
+        $this->adapter->set($key, $value, $ttl = 0);
+
+        $result = $this->adapter->get($key);
+
+        $this->assertTrue($value, $result->getValue());
+    }
+
+    /**
+     * Delete cache for specified $key value or expired cache
+     *
+     * @param   string $key
+     *
+     * @return  bool
+     * @since   1.0
+     */
+    public function testRemove()
+    {
+        $value = 'Stuff';
+        $key   = md5($value);
+        $this->adapter->remove($key);
+        $result = $this->adapter->get($key);
+
+        $this->assertFalse($result->isHit());
+
+    }
+
+    /**
+     * Clear all cache
+     *
+     * @return  bool
+     * @since   1.0
+     */
+    public function testClear()
+    {
+        $keys = array();
+
+        $value = 'Stuff1';
+        $key   = md5($value);
+        $this->adapter->set($key, $value, $ttl = 0);
+        $keys[] = $key;
+
+        $value = 'Stuff2';
+        $key   = md5($value);
+        $this->adapter->set($key, $value, $ttl = 0);
+        $keys[] = $key;
+
+        $value = 'Stuff3';
+        $key   = md5($value);
+        $this->adapter->set($key, $value, $ttl = 0);
+        $keys[] = $key;
+
+        $value = 'Stuff4';
+        $key   = md5($value);
+        $this->adapter->set($key, $value, $ttl = 0);
+        $keys[] = $key;
+
+        $value = 'Stuff5';
+        $key   = md5($value);
+        $this->adapter->set($key, $value, $ttl = 0);
+        $keys[] = $key;
+
+        $value = 'Stuff6';
+        $key   = md5($value);
+        $this->adapter->set($key, $value, $ttl = 0);
+        $keys[] = $key;
+
+        $value = 'Stuff7';
+        $key   = md5($value);
+        $this->adapter->set($key, $value, $ttl = 0);
+        $keys[] = $key;
+
+        $value = 'Stuff8';
+        $key   = md5($value);
+        $this->adapter->set($key, $value, $ttl = 0);
+        $keys[] = $key;
+
+        $value = 'Stuff9';
+        $key   = md5($value);
+        $this->adapter->set($key, $value, $ttl = 0);
+        $keys[] = $key;
+
+        $value = 'Stuff10';
+        $key   = md5($value);
+        $this->adapter->set($key, $value, $ttl = 0);
+        $keys[] = $key;
+
+        $this->adapter->clear();
+
+        foreach ($keys as $key) {
+            $result = $this->adapter->get($key);
+            $this->assertFalse($result->isHit());
+        }
+    }
+
+    /**
+     * Get multiple CacheItems by Key
+     *
+     * @param   array $keys
+     *
+     * @return  array
+     * @since   1.0
+     */
+    public function testGetMultiple()
+    {
+        $keys = array();
+
+        $value = 'Stuff1';
+        $key   = md5($value);
+        $this->adapter->set($key, $value, $ttl = 0);
+        $keys[] = $key;
+
+        $value = 'Stuff2';
+        $key   = md5($value);
+        $this->adapter->set($key, $value, $ttl = 0);
+
+        $value = 'Stuff3';
+        $key   = md5($value);
+        $this->adapter->set($key, $value, $ttl = 0);
+
+        $value = 'Stuff4';
+        $key   = md5($value);
+        $this->adapter->set($key, $value, $ttl = 0);
+
+        $value = 'Stuff5';
+        $key   = md5($value);
+        $this->adapter->set($key, $value, $ttl = 0);
+
+        $value = 'Stuff6';
+        $key   = md5($value);
+        $this->adapter->set($key, $value, $ttl = 0);
+
+        $value = 'Stuff7';
+        $key   = md5($value);
+        $this->adapter->set($key, $value, $ttl = 0);
+
+        $value = 'Stuff8';
+        $key   = md5($value);
+        $this->adapter->set($key, $value, $ttl = 0);
+
+        $value = 'Stuff9';
+        $key   = md5($value);
+        $this->adapter->set($key, $value, $ttl = 0);
+
+        $value = 'Stuff10';
+        $key   = md5($value);
+        $this->adapter->set($key, $value, $ttl = 0);
+        $keys[] = $key;
+
+        $items = $this->adapter->getMultiple($keys);
+
+        $this->assertEquals(2, count($items));
+    }
 
     /**
      * Tears Down
