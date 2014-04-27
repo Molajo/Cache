600525ffacae1fd6332ee756d6e6acf6a99d9111
AmyStephen <AmyStephen@gmail.com>
2014-04-26 19:42:21 -0500

    Packaging

--- .dev/Bootstrap.php
+++ .dev/Bootstrap.php
@@ -6,23 +6,13 @@
  * @copyright  2014 Amy Stephen. All rights reserved.
  * @license    http://www.opensource.org/licenses/mit-license.html MIT License
  */
+$base     = substr(__DIR__, 0, strlen(__DIR__) - 5);
+include_once $base . '/vendor/autoload.php';
 include_once __DIR__ . '/CreateClassMap.php';
 
-if (! defined('PHP_VERSION_ID')) {
-    $version = explode('.', phpversion());
-    define('PHP_VERSION_ID', ($version[0] * 10000 + $version[1] * 100 + $version[2]));
-}
-
-$base     = substr(__DIR__, 0, strlen(__DIR__) - 5);
 $classmap = array();
 
 $results  = createClassMap($base . '/Source', 'Molajo\\Cache\\');
-$classmap = array_merge($classmap, $results);
-$results  = createClassMap($base . '/Factories/Cache', 'Molajo\\Factories\\Cache\\');
-$classmap = array_merge($classmap, $results);
-$results  = createClassMap($base . '/vendor/commonapi/cache', 'CommonApi\\Cache\\');
-$classmap = array_merge($classmap, $results);
-$results  = createClassMap($base . '/vendor/commonapi/exception', 'CommonApi\\Exception\\');
 $classmap = array_merge($classmap, $results);
 
 ksort($classmap);
